255daee3c709814d42c47100b6627fbfcdfcc155
Michaela <Michaela-13>
2022-08-08 10:29:25 +0200

    updated

--- routes/routes.js
+++ routes/routes.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const path = require('path');
 
 // Home page
-router.get('/', (req, res) => {
-    res.status(200).sendFile(path.join(__dirname, '../views', 'index.html'));
-})
+// router.get('/', (req, res) => {
+//     res.status(200).sendFile(path.join(__dirname, '../views', 'index.html'));
+// })
 router.post("/forgot-password", (req, res) => {
     res.status(401).json({
         msg: "You forgot your password"
