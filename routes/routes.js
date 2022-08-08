4994d4efcb4afda57631b2130b29cf8237ebb19b
Michaela <Michaela-13>
2022-08-08 15:36:06 +0200

    login register

--- routes/routes.js
+++ routes/routes.js
@@ -6,10 +6,6 @@ const path = require('path');
 // router.get('/', (req, res) => {
 //     res.status(200).sendFile(path.join(__dirname, '../views', 'index.html'));
 // })
-router.post("/forgot-password", (req, res) => {
-    res.status(401).json({
-        msg: "You forgot your password"
-    })
-});
+
 
 module.exports = router;
